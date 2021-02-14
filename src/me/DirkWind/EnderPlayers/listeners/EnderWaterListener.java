7934516e952b3ccbeb80a45af0730d048a565567
dirkwind <dirkwind707@gmail.com>
2021-02-14 14:36:51 -0600

    fix issues with water damage
    (fixed) players would take rain damage during a storm in places without rain (e.g. deserts, snowy taiga)
    (added) splash and lingering water bottles will now do damage to players with the enderwater power

--- src/me/DirkWind/EnderPlayers/listeners/EnderWaterListener.java
+++ src/me/DirkWind/EnderPlayers/listeners/EnderWaterListener.java
@@ -3,13 +3,20 @@ package me.DirkWind.EnderPlayers.listeners;
 import me.DirkWind.EnderPlayers.Main;
 import me.DirkWind.EnderPlayers.globals.Config;
 import me.DirkWind.EnderPlayers.globals.EnderWater;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.Waterlogged;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.ThrownPotion;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
 
 import java.util.Arrays;
 
-public class EnderWaterListener implements Runnable {
+public class EnderWaterListener implements Runnable, Listener {
 
     private final Main plugin;
     private final Config config;
@@ -18,6 +25,17 @@ public class EnderWaterListener implements Runnable {
         this.plugin = plugin;
         config = Config.getInstance();
         this.plugin.getServer().getScheduler().scheduleSyncRepeatingTask(this.plugin, this, 0L, 10L);
+        this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
+    }
+
+    private boolean containsWater(Block b) {
+        if (b.getType() == Material.WATER) {
+            return true;
+        } else if (b instanceof Waterlogged) {
+            Waterlogged waterlogged = (Waterlogged) b.getBlockData();
+            return waterlogged.isWaterlogged();
+        }
+        return false;
     }
 
     @Override
@@ -25,18 +43,42 @@ public class EnderWaterListener implements Runnable {
         for (Player p : plugin.getServer().getOnlinePlayers()) {
             if (EnderWater.getPlayer(p)) {
                 World w = p.getWorld();
+                Location loc = p.getLocation();
+
+                // check if player is being rained on
                 if (w.hasStorm()) {
-                    if (w.getHighestBlockYAt(p.getLocation()) < p.getLocation().getY())
-                        if (!(config.getHelmetsBlockRain() || config.getFullArmorBlocksWater()) || p.getInventory().getHelmet() == null) {
-                            p.damage(1);
-                            return;
-                        }
+                    if (w.getHighestBlockYAt(loc.clone().add(0, 1, 0)) < loc.getY() && w.getBlockAt(loc.add(0, 1, 0)).isPassable()) {
+                        double temp = w.getTemperature(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+                        if (temp >= 0.15 && temp <= 0.96)
+                            if (!(config.getHelmetsBlockRain() || config.getFullArmorBlocksWater()) || p.getInventory().getHelmet() == null) {
+                                p.damage(1);
+                                return;
+                            }
+                    }
                 }
-                if (w.getBlockAt(p.getLocation()).getType() == Material.WATER ||
-                        w.getBlockAt(p.getLocation().add(0, 1, 0)).getType() == Material.WATER) {
+
+                // check if player is touching water that isn't rain
+                if (containsWater(w.getBlockAt(loc)) || containsWater(w.getBlockAt(loc.add(0, 1, 0)))) {
                     if (!config.getFullArmorBlocksWater() || Arrays.asList(p.getInventory().getArmorContents()).contains(null)) {
                         p.damage(1);
                     }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void playerHitByWater(final ProjectileHitEvent event) {
+        if (event.getEntity() instanceof ThrownPotion) {
+            if (((ThrownPotion) event.getEntity()).getEffects().isEmpty()) {
+                for (Player p : event.getEntity().getWorld().getPlayers()) {
+                    if (EnderWater.getPlayer(p))
+                        if (p.getLocation().distance(event.getEntity().getLocation()) <= 8)
+                            if (!config.getFullArmorBlocksWater() ||
+                                    Arrays.asList(p.getInventory().getArmorContents()).contains(null)) {
+                                p.damage(1);
+                            }
+
                 }
             }
         }
